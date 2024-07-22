892aa2b3d05c370681b2dddf457a6765f87d1ef8
Vivid <suook15@naver.com>
2024-07-23 01:22:42 +0900

    피자 나눠먹기

---
+++ CodingTestLevel0/Pizza.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTestLevel0
+{
+    internal class Pizza
+    {
+
+        static void Main(string[] args)
+        {
+            //머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다.
+            //피자를 나눠먹을 사람의 수 n이 주어질 때,
+            //모든 사람이 피자를 한 조각 이상 먹기 위해 필요한 피자의 수를 return 하는 solution 함수를 완성해보세요.
+
+            int pizzaCount = 1;
+            int pizzaPiece = 7;
+            int people = 0;
+
+
+        }
+
+
+    }
+}
