f8f86d290fc6dc57313ec53ed1da3d66aca1e0fe
Vivid <suook15@naver.com>
2024-08-06 00:51:51 +0900

    코테

---
+++ Example_012/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Example_012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ars = "banana";
+
+            char[] check = { 'a','e','i','o','u' };
+
+
+            for (int i = 0; i < ars.Length; i++)
+            {
+                for (int j = 0; j < check.Length; j++)
+                { 
+                    Console.WriteLine(ars[i].Equals(check[j])); 
+                } 
+                //ars.Contains(check[i]);
+            }
+
+
+
+
+
+        }
+    }
+}
