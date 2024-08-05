eb4d61fc36b7eedd4eb64d5fd9784b0022539d7a
Vivid <suook15@naver.com>
2024-08-05 21:47:01 +0900

    코딩 테스트 연습

--- Example_006/Program.cs
+++ Example_006/Program.cs
@@ -1,6 +1,10 @@
 ﻿namespace Example_006
 {
-
+    /// <summary>
+    /// 날짜 : 2024-08-05
+    /// 문제 : 피자 나눠 먹기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120814
+    /// </summary>
     public class Solution
     {
         public int solution(int n)
---
+++ Example_007/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+
+namespace Example_007
+{
+
+    /// <summary>
+    /// 날짜 : 2024-08-05
+    /// 문제 : 옷가게 할인 받기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120818
+    /// </summary>
+    public class Solution
+    {
+        public int solution(int price)
+        {
+
+            if (price >= 500000)
+            {
+                price -= (int)Math.Ceiling(price * 0.2);
+            }
+            else if (price >= 300000)
+            {
+                price -= (int)Math.Ceiling(price * 0.1);
+            }
+            else if (price >= 100000)
+            {
+                price -= (int)Math.Ceiling(price * 0.05);
+            }
+
+
+            return price;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+        }
+    }
+}
