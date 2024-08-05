eb2ff567d65ba86906b96591e6fd96b29da8de1c
Vivid <suook15@naver.com>
2024-08-05 20:55:20 +0900

    피자 나눠 먹기 알고리즘

---
+++ Example_006/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Example_006
+{
+
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int pizza = 1;
+            int piece = 7;
+
+            while (piece * pizza < n)
+            {
+                pizza++;
+            }
+
+            return pizza;
+        }
+
+
+
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+
+
+            }
+        }
+    }
+}
