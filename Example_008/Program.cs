48fcdeb70dbb47f9129c1a448a88ae9463f09aab
Vivid <suook15@naver.com>
2024-08-05 22:51:34 +0900

    코딩 테스트 연습

---
+++ Example_008/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Example_008
+{
+
+    /// <summary>
+    /// 날짜 : 2024-08-05
+    /// 문제 : 구매 가능한 커피와 잔돈 구하기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120819
+    /// </summary>
+    public class Solution
+    {
+        public int[] solution(int money)
+        {
+            int[] answer = new int[2];
+
+            answer[0] = money / 5500;
+            answer[1] = money % 5500;
+             
+            return answer;
+        }
+    }
+     
+}
---
+++ Example_009/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Example_009
+{
+    public class Solution
+    {
+        public int[] solution(int[] numbers, int num1, int num2)
+        {
+            //정의되어 있는 메소드를 활용
+            int[] answer = numbers.Skip(num1).Take((num2 - num1) + 1).ToArray();
+
+            return answer;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //인덱스를 직접 잘라서 활용
+            int num1 = 1;
+            int num2 = 3;
+            
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int index = (num2 - num1) + 1;
+            int[] arr2 = new int[index];
+            int count = 0;
+
+            for(int i = num1; i <= index; i++)
+            {
+                arr2[count] = arr[i];
+                count++;
+            } 
+        }
+    }
+}
