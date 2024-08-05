6002094c52a8c230d9e88b415f504633f671bd87
Vivid <suook15@naver.com>
2024-08-05 23:15:04 +0900

    문자열 안에 문자열 검사

---
+++ Example_010/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Example_010
+{
+
+    /// <summary>
+    /// 날짜 : 2024-08-05
+    /// 문제 : 문자열 안에 문자열 검사
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120908
+    /// </summary>
+    public class Solution
+    {
+        public int solution(string str1, string str2)
+        {
+            int answer = 2;
+
+            if (str1.Contains(str2))
+            {
+                answer = 1;
+            } 
+            return answer;
+        }
+    }
+}
