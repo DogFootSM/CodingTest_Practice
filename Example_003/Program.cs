01b890bb783ecd0f24b5783da485590a3addceb6
Vivid <suook15@naver.com>
2024-08-05 18:02:58 +0900

    알고리즘

---
+++ Example_003/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Example_003
+{
+    public class Solution
+    {
+        public int[] solution(int[] num_list)
+        {
+            int[] answer = new int[2];
+
+            int oddNum = 0;
+            int evenNum = 0;
+
+            foreach (int num in num_list)
+            {
+                 
+                if (num % 2 == 0)
+                {
+                    answer[0] = evenNum += 1;
+                }
+                else
+                {
+                    answer[1] = oddNum += 1;
+                }
+            }
+
+            Console.WriteLine(answer[0]);
+            Console.WriteLine(answer[1]);
+
+            return answer;
+        }
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int[] numList = [1, 2, 3, 4, 5];
+
+            Console.WriteLine(solution.solution(numList));
+
+
+        }
+    }
+}
---
+++ Example_004/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Example_004
+{
+
+    public class Solution
+    {
+        public int[] solution(int[] num_list)
+        {
+
+            int[] answer = new int[num_list.Length];
+
+            int reverse = num_list.Length - 1;
+
+            for (int i = 0; i < num_list.Length; i++)
+            {
+                answer[i] = num_list[reverse--];
+                 
+            }
+
+
+
+
+            return answer;
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int[] numList = { 1, 1, 1, 1, 1, 2 };
+
+                Solution sol = new Solution();
+
+
+                sol.solution(numList);
+ 
+
+
+
+            }
+        }
+    }
+}
