4fade40edca245db94b9c376404a0d889b966dc2
Vivid <suook15@naver.com>
2024-08-06 23:31:27 +0900

    코딩테스트 연습

---
+++ Level0/Example_022/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Example_022
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 날짜 : 2024-08-06
+        /// 문제 : 최댓값 만들기(2)
+        /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120862#
+        /// </summary>
+        public int solution(int[] numbers)
+        {
+
+            int max = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    //요소가 2개일 때
+                    //Max의 초기값 0이 변하지 않는 것을 방지
+                    if (numbers.Count() < 3)
+                    {
+                        max = numbers.Where(x => x < 0).Count() <= 1 ? numbers[i] * numbers[j] : -1;
+                    }
+
+                    if (numbers[i] * numbers[j] > max)
+                    {
+                        max = numbers[i] * numbers[j];
+                    }
+                } 
+            } 
+            return max; 
+        }
+    }
+}
