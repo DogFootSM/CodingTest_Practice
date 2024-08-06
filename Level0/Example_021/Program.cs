60a74e3ebcb22c70cbe5146ffb6ef97d54ce91fb
Vivid <suook15@naver.com>
2024-08-06 22:15:58 +0900

    코딩테스트_21

---
+++ Level0/Example_021/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Example_021
+{
+    /// <summary>
+    /// 날짜 : 2024-08-06
+    /// 문제 : 자릿수 더하기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120906
+    /// </summary>
+    public class Solution
+    {
+        public int solution(int n)
+        {
+
+            char[] arr = n.ToString().ToCharArray();
+            int answer = 0;
+
+            foreach (char c in arr)
+            {
+                answer += int.Parse(c.ToString());
+            }
+
+
+            return answer;
+        }
+    }
+}
