cbc1a11bd2a494288c1a8d527cc0c8d1e14a63ef
Vivid <suook15@naver.com>
2024-08-06 18:59:31 +0900

    코딩테스트 연습

---
+++ Level0/Example_016/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Example_016
+{
+    public class Solution
+    {
+        public int[] solution(int[] array)
+        {
+
+            int[] answer = new int[2];
+
+            answer[0] = array.Max();
+            answer[1] = Array.IndexOf(array, array.Max());
+
+            //메서드 활용 없이 계산
+            //int[] arr = [1, 6, 8, 3, 2, 5];
+            //int[] ans = new int[2];
+            //int max = 0;
+            //int index = 0;
+
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > max)
+            //    {
+            //        max = arr[i];
+            //        index = i;
+            //    }
+            //}
+
+
+            return answer;
+        }
+    }
+    
+}
