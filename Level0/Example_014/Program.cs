ee237bf77698715a64a82a85630d00605a7ba94c
Vivid <suook15@naver.com>
2024-08-06 17:33:25 +0900

    코딩테스트 연습

---
+++ Level0/Example_014/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Example_014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cipher = "dfjardstddetckdaccccdegk";
+            int code = 4;
+            string answer = "";
+            int count = 0;
+
+            for (int i = 0; i < cipher.Length; i++)
+            {
+                count++;
+                if (count % code == 0)
+                {
+                   answer += cipher[i];
+                } 
+            }
+
+            Console.WriteLine(answer);
+
+        }
+    }
+}
