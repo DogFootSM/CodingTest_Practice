52256b7e2e5b5dcfdbb2ec0c5f251f42ee31a263
Vivid <suook15@naver.com>
2024-08-06 20:15:57 +0900

    코딩테스트 연습

---
+++ Level0/Example_018/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+namespace Example_018
+{
+    /// <summary>
+    /// 날짜 : 2024-08-05
+    /// 문제 : 직각삼각형 출력하기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120823
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            for(int i = 0; i <n; i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
