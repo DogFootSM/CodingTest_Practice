8b5e1eb45941038db2c2b74ac49fa2543692f538
Vivid <suook15@naver.com>
2024-08-06 21:38:37 +0900

    코딩테스트 연습

---
+++ Level0/Example_020/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Example_020
+{
+
+    public class Solution
+    {
+
+        /// <summary>
+        /// 날짜 : 2024-08-06
+        /// 문제 : 주사위 개수 구하기
+        /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120845
+        /// </summary> 
+        public int solution(int[] box, int n)
+        {
+            int answer = 1;
+
+            for (int i = 0; i < box.Length; i++)
+            {
+
+                answer *= (box[i] / n);
+            } 
+            return answer;
+        }
+    }
+}
