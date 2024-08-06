f368c7a0f01c70519932af41ad2c6e7bd8689277
Vivid <suook15@naver.com>
2024-08-06 16:34:27 +0900

    코딩테스트 연습

---
+++ Level0/Example_013/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Example_013
+{
+    internal class Program
+    {
+
+
+        /// <summary>
+        /// 날짜 : 2024-08-06
+        /// 문제 : 문자열 안의 자연수들 덧셈
+        /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120851
+        /// </summary>
+        public class Solution
+        {
+            public int solution(string my_string)
+            {
+                int answer = 0;
+
+                foreach (char num in my_string)
+                {
+
+                    if (num >= 48 && num <= 57)
+                    {
+                        answer += (int)(num - 48);
+                    }
+                }
+
+                return answer;
+            }
+
+           
+        }
+
+         
+    }
+}
