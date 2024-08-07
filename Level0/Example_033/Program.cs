06f46788a620e56621e8107f2062f99aa45627af
Vivid <suook15@naver.com>
2024-08-08 01:32:21 +0900

    코딩테스트 연습

---
+++ Level0/Example_033/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Example_033
+{
+
+
+    /// <summary>
+    /// 날짜 : 2024-08-08
+    /// 문제 : 점의 위치 구하기 
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120841
+    /// </summary>
+    public class Solution
+    {
+        public int solution(int[] dot)
+        {
+            int x = dot[0];
+            int y = dot[1];
+
+
+            if (x > 0)
+            {
+                if (y > 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 4;
+                }
+            }
+
+            else
+            {
+                if (y > 0)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 3;
+                }
+            }
+        }
+    }
+}
---
+++ Level0/Example_034/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Example_034
+{
+
+    /// <summary>
+    /// 날짜 : 2024-08-08
+    /// 문제 : 배열 회전시키기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120844
+    /// </summary>
+    public class Solution
+    {
+        public int[] solution(int[] numbers, string direction)
+        {
+            int[] answer = new int[] { };
+
+            switch (direction)
+            {
+
+                case "right":
+                    int endNum = numbers[numbers.Length - 1];
+
+                    for (int i = numbers.Length - 2; i >= 0; i--)
+                    {
+
+                        numbers[i + 1] = numbers[i];
+
+                        if (i == 0)
+                        {
+                            numbers[i] = endNum;
+                        }
+
+                    }
+
+                    break;
+
+                case "left":
+
+                    int firstNum = numbers[0];
+
+                    for (int i = 0; i < numbers.Length - 1; i++)
+                    {
+
+                        numbers[i] = numbers[i + 1];
+
+                        if (i == numbers.Length - 2)
+                        {
+                            numbers[numbers.Length - 1] = firstNum;
+                        }
+
+                    }
+
+                    break;
+            }
+            return numbers;
+        }
+    }
+}
