80089995992a06781ac2092388bd52e44a3904c0
Vivid <suook15@naver.com>
2024-08-11 02:09:40 +0900

    코딩테스트 연습

---
+++ Level0/Example_047/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Formats.Asn1;
+
+namespace Example_047
+{
+
+    /// <summary>
+    /// 날짜 : 2024-08-11
+    /// 문제 : 역 팩토리얼
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120848
+    /// </summary>
+    public class Solution
+    {
+        public int solution(int n)
+        { 
+            int i = 2;
+             
+            int answer = Fact(n, i);
+
+            return answer;
+        }
+
+        public int Fact(int n, int i)
+        { 
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            if (n / i < i)
+            {
+                return i;
+            } 
+            return Fact((n / i), ++i);
+        }
+
+    }
+}
---
+++ Level0/Example_048/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Example_048
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string my_string = "1000a";
+            int answer = 0;
+
+            List<int> list = new List<int>();
+            string temp = "0";
+            bool isCheck = false;
+
+            for(int i = 0; i< my_string.Length; i++)
+            {
+                //isCheck = int.TryParse(my_string[i].ToString(), out int a);
+
+                if (char.IsNumber(my_string[i]))
+                {
+                    temp += string.Concat(my_string[i]); 
+                }
+                else
+                {
+                    list.Add(int.Parse(temp));
+                    temp = "0";
+                } 
+                //문자를 만나지 않으면 저장이 안됨...
+
+            }
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+
+                answer += i;
+            }
+            Console.WriteLine(answer);
+
+        }
+    }
+}
