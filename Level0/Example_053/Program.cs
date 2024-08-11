a839b21c60a18dd84f32e60e17304dd2a2999075
Vivid <suook15@naver.com>
2024-08-12 00:18:38 +0900

    코딩테스트 연습

--- Level0/Example_053/Program.cs
+++ Level0/Example_053/Program.cs
@@ -1,28 +1,30 @@
 ﻿namespace Example_053
 {
-    internal class Program
+    /// <summary>
+    /// 날짜 : 2024-08-11
+    /// 문제 : 7의 개수
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120912
+    /// </summary>
+    public class Solution
     {
-        static void Main(string[] args)
+        public int solution(int[] array)
         {
             int answer = 0;
             string temp = "";
-            int[] array = { 7, 77, 17 };
 
             for (int i = 0; i < array.Length; i++)
             {
                 temp = array[i].ToString();
-
-                for(int j = 0; j < temp.Length; j++)
+                for (int j = 0; j < temp.Length; j++)
                 {
                     if (int.Parse(temp[j].ToString()) == 7)
                     {
                         answer++;
                     } 
-                } 
+                }
             }
 
-            Console.WriteLine(answer);
-
+            return answer;
         }
     }
 }
---
+++ Level0/Example_054/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Example_054
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "10 20 30 40";
+            int previousNumber = 0;
+            int sum = 0;
+            string sNumber = "";
+            s = string.Concat(s.Split(""));
+
+            //음수 숫자 케이스 생각해보기
+            //공백을 만나기 전까지 sNumber 에 누적
+            //공백을 만나면 sum에 저장
+            //Z를 만나면 previousNumber에 저장된 값을 sum에서 빼기
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsNumber(s[i]))
+                {
+                    sNumber += string.Concat(s[i].ToString());
+
+                }
+                else
+                {
+                    sum += int.Parse(s[i].ToString());
+                    previousNumber = int.Parse(s[i].ToString());
+
+                    sum -= previousNumber;
+                }
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
