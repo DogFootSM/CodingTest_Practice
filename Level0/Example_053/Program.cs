ad42a50f32f386795bdc3842052c04a70703bd0e
Vivid <suook15@naver.com>
2024-08-11 23:35:31 +0900

    코딩테스트 연습

---
+++ Level0/Example_053/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Example_053
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 0;
+            string temp = "";
+            int[] array = { 7, 77, 17 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                temp = array[i].ToString();
+
+                for(int j = 0; j < temp.Length; j++)
+                {
+                    if (int.Parse(temp[j].ToString()) == 7)
+                    {
+                        answer++;
+                    } 
+                } 
+            }
+
+            Console.WriteLine(answer);
+
+        }
+    }
+}
