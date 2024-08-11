62d8ac62f8630c2c08ab03885aab9532ffe1ab4f
Vivid <suook15@naver.com>
2024-08-11 20:41:04 +0900

    코딩테스트 연습

---
+++ Level0/Example_49/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Example_49
+{
+
+    /// <summary>
+    /// 날짜 : 2024-08-11
+    /// 문제 : A로 B만들기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120886
+    /// </summary>
+    public class Solution
+    {
+        public int solution(string before, string after)
+        {
+            int answer = 0;
+
+            int beforeTotal = before.Sum(x => (int)x);
+            int afterTotal = after.Sum(x => (int)x);
+
+            answer = beforeTotal == afterTotal ? 1 : 0;
+
+            return answer;
+        }
+    }
+}
