f48b4a442a0e34b1c67c6cb64d11bdd078d64aa1
Vivid <suook15@naver.com>
2024-08-16 23:28:06 +0900

    코딩 테스트 연습

--- Level0/Example_058/Program.cs
+++ Level0/Example_058/Program.cs
@@ -11,8 +11,8 @@
         public string[] solution(string my_str, int n)
         {
 
-            double dou = (double)my_str.Length / n;
-            int index = (int)Math.Ceiling(dou);
+            double dou = (double)my_str.Length / n;     //문자열의 길이를 n개의 크기만큼 나눈 몫을 저장
+            int index = (int)Math.Ceiling(dou);         //몫을 올림처리하여 배열의 인덱스 확보
 
             string[] answer = new string[index];
 
@@ -21,12 +21,12 @@
 
                 if (my_str.Length < n)
                 {
-                    answer[i] = my_str.Substring(0, my_str.Length);
+                    answer[i] = my_str.Substring(0, my_str.Length);     //남은 문자개수가 잘라야 할 크기보다 작을 경우 마지막까지 저장
                 }
                 else
                 {
-                    answer[i] = my_str.Substring(0, n);
-                    my_str = string.Concat(my_str.Remove(0, n));
+                    answer[i] = my_str.Substring(0, n);             //0 ~ n개까지 문자열 잘라서 저장
+                    my_str = string.Concat(my_str.Remove(0, n));    //자른 문자열을 삭제
                 } 
             } 
             return answer;
