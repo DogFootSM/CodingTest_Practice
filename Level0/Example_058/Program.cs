0466c8b46eb5f7576d4524d40171c203efe48b68
Vivid <suook15@naver.com>
2024-08-16 23:26:35 +0900

    코딩테스트 연습
    문자열 잘라서 배열로 저장

---
+++ Level0/Example_058/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Example_058
+{
+
+    /// <summary>
+    /// 날짜 : 2024-08-16
+    /// 문제 : 문자열 잘라서 배열로 저장하기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/120913
+    /// </summary>
+    public class Solution
+    {
+        public string[] solution(string my_str, int n)
+        {
+
+            double dou = (double)my_str.Length / n;
+            int index = (int)Math.Ceiling(dou);
+
+            string[] answer = new string[index];
+
+            for (int i = 0; i < index; i++)
+            {
+
+                if (my_str.Length < n)
+                {
+                    answer[i] = my_str.Substring(0, my_str.Length);
+                }
+                else
+                {
+                    answer[i] = my_str.Substring(0, n);
+                    my_str = string.Concat(my_str.Remove(0, n));
+                } 
+            } 
+            return answer;
+        }
+    }
+}
