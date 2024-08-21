9bb50b98186994163e2048bfec7acfe65ec2099d
Vivid <suook15@naver.com>
2024-08-21 19:04:16 +0900

    코딩 테스트 연습

---
+++ Level0/Example_062/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Example_062
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int balls = 5;
+            int share = 3;
+
+            int left = balls - share;
+            int n = 1;
+            int m = 1;
+            int answer = 0;
+ 
+            //try catch 문 사용?
+            for(int i = balls; i > share; i--)
+            {
+                n *= i;
+ 
+            }
+
+            for(int j = left; j >= 1; j--)
+            {
+                m *= j;
+ 
+            }
+            answer = n / m;
+
+            Console.WriteLine(answer); 
+        }
+    }
+}
