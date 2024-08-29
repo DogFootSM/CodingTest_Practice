7727dc39034d7badb9778d0a7d31eed46fab0685
Vivid <suook15@naver.com>
2024-08-29 09:46:01 +0900

    코딩 테스트 연습

--- Level1/Example_001/Program.cs
+++ Level1/Example_001/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string[] s; 
+            s = Console.ReadLine().Split(' ');
+
+            int n = int.Parse(s[0]);
+            int m = int.Parse(s[1]);
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+             
         }
     }
 }
