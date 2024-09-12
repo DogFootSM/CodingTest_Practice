d528315e8dc32b9bf405e8585db1451b69b909fd
Vivid <suook15@naver.com>
2024-09-12 21:03:18 +0900

    코딩테스트 연습
    이상한 문자 만들기 12930

--- Level1/Example_007/Program.cs
+++ Level1/Example_007/Program.cs
@@ -2,56 +2,64 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// 날짜 : 2024-09-06
+        /// 문제 : 문자열 나누기
+        /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/140108
+        /// </summary>
+        public class Solution
         {
-            string s = "aaab";
-
-
-            List<string> list = new List<string>();
-            int trueCount = 1;
-            int falseCount = 0;
- 
-            string empty = "";
-
-            while (s.Length > 0)
+            public int solution(string s)
             {
- 
-                for(int i = 1; i < s.Length; i++)
-                {
-                    if(s[0].ToString() == s[i].ToString())
-                    {
-                        trueCount++;
-                    }
-                    else
-                    {
-                        falseCount++;
-                         
-                    }
+                int answer = 0;
+                List<string> list = new List<string>();
+                int trueCount = 1;
+                int falseCount = 0;
 
-                    if (trueCount == falseCount)
+                string empty = "";
+
+                while (s.Length > 0)
+                {
+
+                    for (int i = 1; i < s.Length; i++)
                     {
-                        for(int c = 0;  c < trueCount + falseCount; c++)
+                        if (s[0].ToString() == s[i].ToString())
                         {
-                            empty += s[c].ToString();
+                            trueCount++;
                         }
-                        list.Add(empty);
-                        s = s.Remove(0, empty.Length);
-                        trueCount = 1;
-                        falseCount = 0;
-                        i = 0;
-                        empty = "";
+                        else
+                        {
+                            falseCount++;
+
+                        }
+
+                        if (trueCount == falseCount)
+                        {
+                            for (int c = 0; c < trueCount + falseCount; c++)
+                            {
+                                empty += s[c].ToString();
+                            }
+                            list.Add(empty);
+                            s = s.Remove(0, empty.Length);
+                            trueCount = 1;
+                            falseCount = 0;
+                            i = 0;
+                            empty = "";
+                        }
                     }
-                }
 
-                if(s.Length >= 1 && trueCount != falseCount)
-                {
-                    list.Add(s);
-                    break;
-                }
+                    if (s.Length >= 1 && trueCount != falseCount)
+                    {
+                        list.Add(s);
+                        break;
+                    }
 
+                }
+                answer = list.Count;
+
+
+                return answer;
             }
- 
-
         }
     }
 }
---
+++ Level1/Example_008/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Example_008
+{
+    /// <summary>
+    /// 날짜 : 2024-09-12
+    /// 문제 : 이상한 문자 만들기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/12930
+    /// </summary>
+    public class Solution
+    {
+        public string solution(string s)
+        {
+            string temp = "";
+            string[] arr = s.Split(" ");
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {   
+                    //짝수, 홀수 인덱스 구분
+                    if (j % 2 == 0)
+                    {
+                        temp += arr[i][j].ToString().ToUpper();
+                    }
+                    else
+                    {
+                        temp += arr[i][j].ToString().ToLower();
+                    }
+                }
+
+                //공백을 기준으로 잘랐던 문자열 공백 재조합
+                if (i != arr.Length - 1)
+                {
+                    temp += " ";
+                }
+            }
+            return temp;
+        }
+    }
+}
