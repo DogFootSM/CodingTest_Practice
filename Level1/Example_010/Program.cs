c55f09b643434aa06ab0b539ea383b4c2f1dc009
Vivid <suook15@naver.com>
2024-09-14 01:13:17 +0900

    코딩테스트 연습
    소수 찾기

--- Level1/Example_010/Program.cs
+++ Level1/Example_010/Program.cs
@@ -1,13 +1,24 @@
-﻿namespace Example_010
+﻿using System.Xml.Linq;
+
+namespace Example_010
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          
-   
+            int n = 10;
+
+
+            bool[] primeNum = new bool[n + 1];
+            
+            for(int i = 2; i < primeNum.Length; i++)
+            {
+                primeNum[i] = true;
+            }
+             
+             
         }
-   
+
 
     }
 }
