603932022a97a30db1a382ea16cb11334bbcb14c
Vivid <suook15@naver.com>
2024-10-14 01:54:03 +0900

    [10/14] 코딩 테스트

---
+++ Level1/Example_12910/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Example_12910
+{
+    using System.Collections.Generic;
+
+
+    /// <summary>
+    /// 날짜 : 2024-10-14
+    /// 문제 : 나누어 떨어지는 숫자 배열
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/12910
+    /// </summary>
+    public class Solution
+    {
+        public int[] solution(int[] arr, int divisor)
+        {
+            int[] answer;
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //나누어 떨어지는 수 검사
+                if (arr[i] % divisor == 0)
+                {
+                    list.Add(arr[i]);
+                }
+            }
+
+            //나누어 떨어지는 숫자가 없었을 경우 -1 추가
+            if (list.Count == 0)
+            {
+                list.Add(-1);
+            }
+
+            answer = list.ToArray();
+
+            //배열을 순회하며 오름차순 정렬
+            for (int i = 0; i < answer.Length; i++)
+            { 
+                for (int j = 0; j < (answer.Length - 1) - i; j++)
+                { 
+                    if (answer[j] > answer[j + 1])
+                    {
+                        int temp = answer[j];
+                        answer[j] = answer[j + 1];
+                        answer[j + 1] = temp;
+
+                    }
+                }
+            } 
+            return answer;
+        }
+    }
+}
---
+++ Level1/Example_12935/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Example_12935
+{
+
+    /// <summary>
+    /// 날짜 : 2024-10-14
+    /// 문제 : 제일 작은 수 제거하기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/12935
+    /// </summary>
+    public class Solution
+    {
+        public int[] solution(int[] arr)
+        {
+
+            int[] answer;
+
+            //배열에 들어있는 값이 1개이면 -1 반환
+            if (arr.Length == 1)
+            {
+                answer = new int[1];
+                answer[0] = -1;
+                return answer;
+            }
+
+            List<int> list = new List<int>();
+
+            //arr 배열을 list로 변환
+            list = arr.ToList();
+
+            //list에서 가장 작은 값 제거
+            list.Remove(list.Min());
+
+            //list > int 배열로 변환
+            answer = list.ToArray();
+             
+            return answer;
+        }
+    }
+}
