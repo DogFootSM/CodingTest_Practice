58e9f7e3ab38475e47b54b84efd3ba38091b4088
Vivid <suook15@naver.com>
2024-10-14 12:06:09 +0900

    [10/14] 코딩 테스트

---
+++ Level1/Example_12922/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Example_12922
+{
+    /// <summary>
+    /// 날짜 : 2024-10-14
+    /// 문제 : 수박수박수
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/12922
+    /// </summary>
+    public class Solution
+    {
+        public string solution(int n)
+        {
+            string answer = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                answer += string.Concat((i % 2 == 0 ? '수' : '박'));
+            }
+
+            return answer;
+        }
+    }
+}
