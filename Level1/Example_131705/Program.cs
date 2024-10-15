2a46eb97f321bdb2fd2cdb906a1f8e6f012b189c
Vivid <suook15@naver.com>
2024-10-15 09:19:49 +0900

    [10/15] 코딩 테스트

---
+++ Level1/Example_131705/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Example_131705
+{
+
+    /// <summary>
+    /// 날짜 : 2024-10-15
+    /// 문제 : 삼총사
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/131705
+    /// </summary>
+    public class Solution
+    {
+        public int solution(int[] number)
+        {
+            int answer = 0;
+
+            //첫 번째 수
+            for (int i = 0; i < number.Length; i++)
+            {
+                //두 번쨰 수
+                for (int j = i + 1; j < number.Length; j++)
+                {
+                    //세 번째 수
+                    for (int k = j + 1; k < number.Length; k++)
+                    {
+                        //1,2,3 자리의 수를 더한 값이 0이면 개수 증가
+                        if (number[i] + number[j] + number[k] == 0)
+                        {
+                            answer++;
+                        } 
+                    } 
+                } 
+            } 
+            return answer;
+        }
+    }
+}
---
+++ Level1/Example_68935/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Example_68935
+{
+
+    /// <summary>
+    /// 날짜 : 2024-10-14
+    /// 문제 : 3진법 뒤집기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/68935
+    /// </summary>
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+            List<int> list = new List<int>();
+
+            while (true)
+            {
+                //3으로 나누어 떨어지지 않을 경우 몫을 리스트에 추가
+                if (n / 3 == 0)
+                {
+                    list.Add(n);
+                    break;
+                }
+
+                //3으로 나눈 나머지를 리스트에 저장
+                list.Add(n % 3);
+                n /= 3;
+            }
+
+            list.Reverse();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                //3의 i승 만큼 나머지 곱해서 10진수로 변환
+                answer += (list[i] * (int)Math.Pow(3, i));
+            }
+
+
+            return answer;
+        }
+    }
+}
