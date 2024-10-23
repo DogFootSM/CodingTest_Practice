c21206db4f3848a80164d558ee22b72b1db3e036
Vivid <suook15@naver.com>
2024-10-24 01:13:48 +0900

    [10/24] 코딩 테스트

---
+++ Level1/Example_142086/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Example_142086
+{
+
+    /// <summary>
+    /// 날짜 : 2024-10-24
+    /// 문제 : 가장 가까운 같은 글자
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/142086
+    /// </summary>
+    public class Solution
+    {
+        public int[] solution(string s)
+        {
+            int[] answer = new int[s.Length];
+
+            answer[0] = -1;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (s[j].Equals(s[i]))
+                    {
+                        answer[i] = i - j;
+                        break;
+                    }
+                    else
+                    {
+                        answer[i] = -1;
+                    }
+                } 
+            } 
+            return answer;
+        }
+    }
+}
---
+++ Level1/Example_42748/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Example_42748
+{
+
+    /// <summary>
+    /// 날짜 : 2024-10-24
+    /// 문제 : K 번째 수
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/42748
+    /// </summary>
+    public class Solution
+    {
+        public int[] solution(int[] array, int[,] commands)
+        {
+
+            int[] answer = new int[commands.GetLength(0)];
+            List<int> temp = new List<int>();
+
+            for (int i = 0; i < commands.GetLength(0); i++)
+            {
+                int f = commands[i, 0] - 1;
+                int l = commands[i, 1];
+
+                for (int j = f; j < l; j++)
+                {
+                    temp.Add(array[j]);
+                } 
+
+                temp.Sort();
+                answer[i] = temp[commands[i, 2] - 1];
+                temp.Clear(); 
+            } 
+            return answer;
+        }
+    }
+}
