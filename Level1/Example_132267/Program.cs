2e09f02094a8c1655d54f035fe8c8e4bf109e483
Vivid <suook15@naver.com>
2024-10-25 00:44:35 +0900

    [10/24] 코딩 테스트

---
+++ Level1/Example_132267/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Example_132267
+{
+
+    /// <summary>
+    /// 날짜 : 2024-10-24
+    /// 문제 : 콜라 문제
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/132267
+    /// </summary> 
+    public class Solution
+    {
+        public int solution(int a, int b, int n)
+        {
+            int answer = 0; 
+            answer = Change(a, b, n, answer); 
+            return answer;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a">최소 교환 개수</param>
+        /// <param name="b">교환 시 돌려받을 개수</param>
+        /// <param name="n">빈병의 총 개수</param>
+        /// <param name="result">총 교환한 빈병 개수</param>
+        /// <returns></returns>
+        public int Change(int a, int b, int n, int result)
+        {
+            //보유한 빈병이 최소 교환 개수보다 큰 경우에 교환 진행
+            if (n >= a)
+            {
+                result += (n / a) * b;
+            }
+            //보유중인 빈병이 부족하면 그동안 교환한 개수 반환
+            else
+            {
+                return result;
+            }
+
+            return Change(a, b, ((n / a) * b) + (n % a), result);
+        }
+
+    }
+}
---
+++ Level1/Example_134240/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+namespace Example_134240
+{
+
+    /// <summary>
+    /// 날짜 : 2024-10-25
+    /// 문제 : 푸드 파이트 대회
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/134240
+    /// </summary> 
+    public class Solution
+    {
+        public string solution(int[] food)
+        {
+            string answer = "";
+
+            //food 요소의 절반 만큼 i + 1 값을 붙임
+            for (int i = 0; i < food.Length - 1; i++)
+            {
+                for (int j = 0; j < food[i + 1] / 2; j++)
+                {
+                    answer += (i + 1).ToString();
+                }
+            }
+
+            //문자열을 char 배열에 할당 후 뒤집음
+            char[] crr = answer.ToCharArray();
+            Array.Reverse(crr);
+            answer += "0";
+
+            //뒤집은 문자를 문자열에 이어붙임
+            for (int i = 0; i < crr.Length; i++)
+            { 
+                answer += crr[i].ToString();
+            }
+
+            return answer;
+        }
+
+    }
+}
