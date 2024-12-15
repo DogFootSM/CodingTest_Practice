4499534153688082b0e15f74b909273093dbd57d
Vivid <suook15@naver.com>
2024-12-15 13:58:43 +0900

    JetBrain Test

---
+++ Level1/Example_12977/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Example_12977
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine(double.MaxValue);
+        }
+    }
+}
---
+++ Level1/Example_136798/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.Collections;
+
+namespace Example_136798
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            i++;
+        }
+
+    }
+
+}
