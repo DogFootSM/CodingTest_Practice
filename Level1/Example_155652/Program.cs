a250383711c0beb94141645c81935b1e8f3a31aa
Vivid <suook15@naver.com>
2025-02-25 15:36:24 +0900

    [02.25] 155652 - 코테 풀이
    1. 둘만의 암호

---
+++ Level1/Example_155652/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/// <summary>
+/// 날짜 : 2025.02.25
+/// 문제 : 둘만의 암호
+/// URL : https://school.programmers.co.kr/learn/courses/30/lessons/155652#
+/// </summary>
+public class Solution {
+    public string solution(string s, string skip, int index) {
+        string answer = "";
+        
+        char[] sToCharArr = s.ToCharArray();
+        char[] skipToCharArr = skip.ToCharArray();
+        List<char> charList = new List<char>();
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (skipToCharArr.Contains((char)(97 + i))) continue;
+            
+            charList.Add((char)(97 + i)); 
+        }
+
+        for (int i = 0; i < sToCharArr.Length; i++)
+        {
+            int skipIndex = charList.IndexOf(sToCharArr[i]);
+
+            if (skipIndex + index >= charList.Count)
+            {
+                skipIndex =  (skipIndex + index) % charList.Count;
+            }
+            else
+            {
+                skipIndex = skipIndex + index;
+            }
+            sToCharArr[i] = charList[skipIndex]; 
+        }
+        
+        answer = string.Join("", sToCharArr);
+        
+        return answer;
+    }
+}
