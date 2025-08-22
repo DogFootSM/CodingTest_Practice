493e9d7e913d8626402312ead36141a79e7e7543
Vivid <suook15@naver.com>
2025-08-22 22:47:31 +0900

    [Etc] 코테 12951 문제 풀이
    1. JadenCase 문자열 만들기

---
+++ Programmers_Level1/Example_131128/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+
+public class NumberPartner
+{
+    private static void Main(string[] args)
+    {
+        //같은 숫자가 있을 경우 그것을 모아서 가장 큰 수로 만들기
+        //같은 수가 0만 있을 경우 0
+        //같은 수가 없으면 -1
+        //자릿수가 300만
+         
+        
+        string x = "12222";
+        string y = "21111";
+        string answer = "";
+
+        long[] xNums = new long[10];
+        long[] yNums = new long[10];
+  
+        //시간을 잡아먹는 요소일것으로 예상
+        for (int i = 0; i < 10; i++)
+        {
+            xNums[i] = x.Where(x => (x - 48) == i).Count(); 
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            yNums[i] = y.Where(x => (x - 48) == i).Count();
+        }
+
+        for (int i = xNums.Length -1; i >= 0; i--)
+        {
+            if(xNums[i] == 0 || yNums[i] == 0) continue;
+
+            long min = Math.Min(xNums[i], yNums[i]);
+            
+            for (int j = 0; j < min; j++)
+            {
+                answer += i; 
+            }
+             
+        }
+
+        if (answer == "")
+        {
+            answer = "-1";
+        }
+        else if (int.Parse(answer) == 0)
+        {
+            answer = "0";
+        } 
+        
+        Console.WriteLine(answer);
+        
+    }   
+}
---
+++ Programmers_Level2/Level2/Ex12951.cs
@@ -0,0 +1,40 @@
+﻿namespace Level2;
+
+
+/// <summary>
+/// 날짜 : 2025-08-22
+/// 문제 : JadenCase 문자열 만들기
+/// URL : https://school.programmers.co.kr/learn/courses/30/lessons/12951
+/// </summary>
+public class Ex12951
+{
+    public string solution(string s) {
+        string answer = "";
+        
+        string[] sWords = s.Split(' ');
+        
+        for(int i = 0; i < sWords.Length; i++)
+        {
+            sWords[i] = sWords[i].ToLower();
+            
+            if(sWords[i].Length == 0) continue;
+            if(int.TryParse(sWords[i][0].ToString(), out int num)) continue;
+             
+            string first = sWords[i][0].ToString().ToUpper();
+            
+            sWords[i] = first + sWords[i].Remove(0, 1);
+        }
+        
+        for(int i = 0; i < sWords.Length; i++)
+        {
+            answer += sWords[i];
+            
+            if(i < sWords.Length - 1)
+            {
+                answer += " ";
+            } 
+        }
+         
+        return answer;
+    }
+}
