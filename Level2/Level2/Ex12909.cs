58b12a4cacb5b05cd1fc98dd3952fec1e478e671
Vivid <suook15@naver.com>
2025-08-22 10:26:41 +0900

    [Etc] 코테 12909 문제 풀이
    1. 올바른 괄호

---
+++ Level2/Level2/Ex12909.cs
@@ -0,0 +1,38 @@
+﻿namespace Level2;
+
+
+/// <summary>
+/// 날짜 : 2025-08-22
+/// 문제 : 올바른 괄호
+/// URL : https://school.programmers.co.kr/learn/courses/30/lessons/12909
+/// </summary>
+public class Ex12909
+{
+    public bool solution(string s)
+    {
+        bool answer = true;
+
+        Stack<char> bracketStack = new Stack<char>();
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i].Equals(')'))
+            {
+                bracketStack.Push(s[i]);
+            }
+            else
+            {
+                if (bracketStack.Count > 0)
+                {
+                    bracketStack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return bracketStack.Count == 0;
+    }
+}
--- Level2/Level2/Ex12939.cs
+++ Level2/Level2/Ex12939.cs
@@ -1,5 +1,10 @@
 ﻿namespace Level2;
 
+/// <summary>
+/// 날짜 : 2025-08-22
+/// 문제 : 최대값과 최솟값
+/// URL : https://school.programmers.co.kr/learn/courses/30/lessons/12939
+/// </summary>
 public class Ex12939
 {
     public string solution(string s)
