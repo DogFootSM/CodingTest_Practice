22891b4bb29e7a0d8d89b1cd74351600d6dc5dba
Vivid <suook15@naver.com>
2025-08-26 10:49:16 +0900

    [08/26] 12911, 70129 - 코테 문제 풀이
    1. 이진 변환 반복하기
    2. 다음 큰 숫자 구하기

---
+++ Programmers_Level2/Level2/Ex12911.cs
@@ -0,0 +1,61 @@
+﻿namespace Level2;
+
+
+/// <summary>
+/// 날짜 : 2025-08-26
+/// 문제 : 다음 큰 숫자
+/// URL : https://school.programmers.co.kr/learn/courses/30/lessons/12911
+/// </summary>
+public class Ex12911
+{
+    public int solution(int n) 
+    {
+        int answer = 0;
+        int nValue = n;
+        int multiple = 1;
+        int temp = 0;
+        int increaseNum = n;
+        
+        int compTemp = 0;
+
+        while(nValue >= 1)
+        {
+            if(nValue % 2 != 0)
+            {
+                temp += (nValue % 2) * multiple;
+                multiple *= 10;
+            }
+
+            nValue /= 2;
+        }
+
+        while(increaseNum < int.MaxValue)
+        {
+            increaseNum += 1;
+            nValue = increaseNum;
+            compTemp = 0;
+            multiple = 1;
+            
+            while(nValue >= 1)
+            {
+                if(nValue % 2 != 0)
+                {
+                    compTemp += (nValue % 2) * multiple;
+                    multiple *= 10;
+                }
+                
+                nValue /= 2;
+            }
+            
+            if(compTemp == temp && increaseNum > n)
+            {
+                answer = increaseNum;
+                break;
+            } 
+        }
+         
+        return answer;
+    }
+    
+    
+}
---
+++ Programmers_Level2/Level2/Ex70129.cs
@@ -0,0 +1,42 @@
+﻿namespace Level2;
+
+/// <summary>
+/// 날짜 : 2025-08-26
+/// 문제 : 이진 변환 반복하기
+/// URL : https://school.programmers.co.kr/learn/courses/30/lessons/70129
+/// </summary>
+public class Ex70129
+{
+    public int[] solution(string s) {
+        int[] answer = new int[2];
+        int temp = 0;
+        int changeCount = 0;
+        int numCount = 0;
+        int sNum = 0;
+        
+        while(s.Length > 1)
+        {
+            numCount = s.Where(x => x != '0').Count();
+            
+            if(s.Length - numCount >= 0)
+            {
+                temp += s.Length - numCount;
+            }
+            
+            changeCount++;
+            sNum = numCount;
+            s = "";
+            
+            while(sNum >= 1)
+            {
+                s += sNum % 2;
+                sNum /= 2;
+            } 
+        }
+        
+        answer[0] = changeCount;
+        answer[1] = temp;
+         
+        return answer;
+    }
+}
