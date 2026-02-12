f6da160a04e7eb3c9a00828acaa122d6d1c189e4
Vivid <suook15@naver.com>
2026-02-13 01:41:41 +0900

    [02.11] 문제 풀이
    1. 기사단원의 무기 : 136798
    2. 소수 만들기 : 12977

--- Programmers_Level1/Example_12977/Program.cs
+++ Programmers_Level1/Example_12977/Program.cs
@@ -1,10 +1,49 @@
 ﻿namespace Example_12977
 {
-    internal class Program
+    /// <summary>
+    /// 날짜 : 2026.02.11
+    /// 문제 : 소수 만들기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/12977
+    /// </summary>
+    class Solution
     {
-        static void Main(string[] args)
+        public int solution(int[] nums)
         {
-             Console.WriteLine(double.MaxValue);
+            int answer = 0;
+        
+            for(int i = 0; i < nums.Length - 2; i++)
+            {
+                for(int j = i + 1; j < nums.Length - 1; j++)
+                {
+                    for(int k = j + 1; k < nums.Length; k++)
+                    {
+                        int number = nums[i] + nums[j] + nums[k];
+                    
+                        if(IsPrimeNumber(number))
+                        {
+                            answer++;
+                        }
+                    }
+            
+                }
+            
+            }
+        
+
+            return answer;
+        }
+    
+        public bool IsPrimeNumber(int number)
+        {
+            for(int i = 2; i * i <= number; i++)
+            {
+                if(number % i == 0)
+                {
+                    return false;
+                }
+            }
+        
+            return true;
         }
     }
 }
--- Programmers_Level1/Example_136798/Program.cs
+++ Programmers_Level1/Example_136798/Program.cs
@@ -4,15 +4,51 @@ using System.Collections;
 
 namespace Example_136798
 {
-
-    internal class Program
+    /// <summary>
+    /// 날짜 : 2026.02.11
+    /// 문제 : 기사단원의 무기
+    /// URL : https://school.programmers.co.kr/learn/courses/30/lessons/136798
+    /// </summary>
+    public class Example_136798
     {
-        static void Main(string[] args)
+        public void solution(int number, int limit, int power)
         {
-            int i = 5;
-            i++;
+            int div = 0;
+            int iron = 0;
+            
+            for (int i = 1; i <= number; i++)
+            {
+                div = GetDivNumber(i);
+
+                if (div > limit)
+                {
+                    iron += limit;
+                }
+                else
+                {
+                    iron += div;
+                }
+            } 
         }
 
-    }
+        private int GetDivNumber(int number)
+        {
+            int count = 0;
 
+            for (int i = 1; i * i <= number; i++)
+            {
+                if (i * i == number)
+                {
+                    count++;
+                }
+                else if (number % i == 0)
+                {
+                    count += 2;
+                }
+            }
+
+            return count;
+        }
+        
+    }
 }
