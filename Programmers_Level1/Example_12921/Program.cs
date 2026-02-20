4b18b35b4c63e88f857fad6bc9ee14084fc3dbc9
Vivid <suook15@naver.com>
2026-02-20 10:18:49 +0900

    [02.20] 코테 문제 풀이
    1. 소수 찾기 문제 풀이
    2. 햄버거 만들기

---
+++ Programmers_Level1/Example_12921/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+/// <summary>
+/// 날짜 : 2026.02.20
+/// 문제 : 소수찾기
+/// URL : https://school.programmers.co.kr/learn/courses/30/lessons/12921
+/// </summary>
+public class Example_12921
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+        
+        bool[] primes = new bool[n + 1];
+
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (!primes[i])
+            {
+                for (int j = i * i; j <= n; j += i)
+                {
+                    primes[j] = true;
+                }
+            } 
+        }
+
+        for (int i = 2; i <= n; i++)
+        {
+            if (!primes[i]) answer++;
+        }
+
+        return answer;
+    }
+    
+    // public int solution(int n)
+    // {
+    //     int answer = 0;
+    //     
+    //     for (int i = 2; i <= n; i++)
+    //     {
+    //         if (IsPrimeNumber(i)) answer++;
+    //     }
+    //
+    //     return answer;
+    // }
+    //
+    // public bool IsPrimeNumber(int number)
+    // {
+    //     for (int i = 2; i * i <= number; i++)
+    //     {
+    //         if (number % i == 0) return false;
+    //     }
+    //
+    //     return true;
+    // }
+}
---
+++ Programmers_Level1/Example_133502/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 날짜 : 2026.02.20
+/// 문제 : 햄버거만들기
+/// URL : https://school.programmers.co.kr/learn/courses/30/lessons/133502?language=csharp#
+/// </summary>
+public class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        
+        Stack<int> inStack = new Stack<int>();
+        List<int> indexList = new List<int>();
+        
+        for(int i = 0; i < ingredient.Length; i++)
+        {
+            inStack.Push(ingredient[i]);
+            
+            if(inStack.Count > 3 && inStack.Peek() == 1)
+            {
+                indexList.Clear();
+                int prevIngredient = 3;
+                int index = i - 1;
+                indexList.Add(i);
+                
+                for(int j = index; j >= 0; j--)
+                {
+                    if(ingredient[j] == prevIngredient)
+                    {
+                        prevIngredient--;
+                        indexList.Add(j);
+
+                        if(indexList.Count == 4)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if(ingredient[j] != 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+                
+                if(indexList.Count == 4)
+                {
+                    for(int k = 0; k < indexList.Count; k++)
+                    {
+                        ingredient[indexList[k]] = 0;
+                    }
+                    
+                    answer++;
+                }
+            } 
+        }
+         
+        return answer;
+    }
+}
