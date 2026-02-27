e1ec66a87227c9255f1a2465a565f8b721256c3e
Vivid <suook15@naver.com>
2026-02-27 13:15:00 +0900

    [02.27] 코테 문제 풀이
    1. 유연근무제

---
+++ Programmers_Level1/Example_388351/Program.cs
@@ -0,0 +1,40 @@
+﻿
+/// <summary>
+/// 날짜 : 2026.02.27
+/// 문제 : 유연근무제
+/// URL : https://school.programmers.co.kr/learn/courses/30/lessons/388351
+/// </summary>
+public class Solution {
+    public int solution(int[] schedules, int[,] timelogs, int startday) {
+        int answer = 0;
+        int deadline = 0;
+        
+        for(int i = 0; i < schedules.Length; i++)
+        {
+            deadline = startday - 1;
+            
+            int schedule = schedules[i] + 10;
+            
+            int hour = ((schedule / 100) + (schedule % 100) / 60) * 100;
+            int minute = (schedule % 100) % 60; 
+            
+            schedule = hour + minute;
+            
+            for(int j = 0; j < timelogs.GetLength(1); j++)
+            {
+                if(timelogs[i,j] > schedule)
+                {
+                    if(deadline < 5)
+                    {
+                        answer++;
+                        break;
+                    } 
+                } 
+                
+                deadline = ++deadline % 7;
+            }   
+        }   
+        
+        return schedules.Length - answer;
+    }
+}
