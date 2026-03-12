2de51b9da0432a0c931d24756f907a54cf69b623
Vivid <suook15@naver.com>
2026-03-12 11:47:06 +0900

    [03.12] 코테 문제 풀이
    1. 카펫

---
+++ Programmers_Level2/Level2/Ex42842.cs
@@ -0,0 +1,33 @@
+﻿namespace Level2;
+
+/// <summary>
+/// 날짜 : 2026.03.12
+/// 문제 : 카펫
+/// URL : https://school.programmers.co.kr/learn/courses/30/lessons/42842
+/// </summary>
+public class Ex42842
+{
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int size = brown + yellow;
+        
+        for(int i = 3; i * i <= size; i++)
+        {
+            //w 전체 가로 길이
+            int w = size / i;
+            
+            //yHeight 전체 높이
+            int yWidth = w - 2;
+            int yHeight = (yellow / yWidth) + 2;
+             
+            if(yellow % yWidth == 0 && i == yHeight)
+            {
+                answer[0] = w; 
+                answer[1] = yHeight;
+                break;
+            } 
+        } 
+        return answer;
+    }
+}
